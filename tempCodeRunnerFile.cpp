7cce8d7a73328f94857df6ffde0618490fc00914
parijain2208 <jainps2208@gmail.com>
2025-11-17 22:41:45 +0530

    day_2_Sheet

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"enter first number";
+    cin>>n1;
+    cout<<"enter second number";
+    cin>>n2;
+    cout<<"sum"<<n1+n2<<endl;
+    cout<<"diif"<<n1-n2<<endl;
+    cout<<"product"<<n1*n2<<endl;
+    cout<<"division"<<n1/n2<<endl;
+}
